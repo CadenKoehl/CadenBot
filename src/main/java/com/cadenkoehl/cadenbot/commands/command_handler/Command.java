78d00b946e3b4a7143b67769adb9811ef713dd49
cadenkoehl <caden@etherscapes.com>
2021-03-06 08:52:41 -1000

    Added Command.getByName() method

--- src/main/java/com/cadenkoehl/cadenbot/commands/command_handler/Command.java
+++ src/main/java/com/cadenkoehl/cadenbot/commands/command_handler/Command.java
@@ -1,9 +1,7 @@
 package com.cadenkoehl.cadenbot.commands.command_handler;
 
-import com.cadenkoehl.cadenbot.util.Constants;
 import com.cadenkoehl.cadenbot.util.exceptions.IncorrectUsageException;
 import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 public abstract class Command {
 
@@ -20,4 +18,12 @@ public abstract class Command {
     public abstract String getUsage(String prefix);
 
     public abstract String[] getAliases();
+
+
+    public static Command getByName(String name) {
+        for(Command cmd : CommandHandler.commands) {
+            if(cmd.getName().equalsIgnoreCase(name)) return cmd;
+        }
+        return null;
+    }
 }
